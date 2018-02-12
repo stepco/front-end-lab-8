9fd799c548b6f23ab1c6e7d8944fc3e7f037ee9d
Stepan Romanchuk <stepco@i.ua>
2018-02-12 15:44:34 +0200

    final

--- homework_06/js/task1.js
+++ homework_06/js/task1.js
@@ -1,30 +1,30 @@
-var sideA = Number(prompt("Введіть довжину першої сторони трикутника"));
-var sideB = Number(prompt("Введіть довжину другої сторони трикутника"));
-var sideC = Number(prompt("Введіть довжину третьої сторони трикутника"));
-var perymetrOfTriangle;
-var squareOfTriangle ;
-var typeOfTriangle;
+let a = Number(prompt("enter number"));
+let b = Number(prompt("enter number"));
+let c = Number(prompt("enter number"));
+let perymetrOfTriangle;
+let squareOfTriangle;
+if (a && b && c && a > 0 && b > 0 && c > 0 && a + b > c && b + c > a & a + c > b) {
+      perymetrOfTriangle = a + b + c;
+      squareOfTriangle = Math.sqrt(perymetrOfTriangle*(perymetrOfTriangle - a)*(perymetrOfTriangle - b)*(perymetrOfTriangle - c));
+ } 
 
-if(sideA === sideB && sideC === sideB){
+
+if(a === b && b === c){
   typeOfTriangle = 'Equilateral';
-} else if (sideA === sideB || sideA === sideC || sideC === sideB){
+} else if (a === b || a === c || c === b){
   typeOfTriangle = 'Isosceles';
 } else if (
-             (sideA >  sideB === true && sideA > sideC === true) && Math.pow(sideA, 2) === Math.pow(sideB, 2) + Math.pow(sideC, 2) ||
-             (sideB > sideA === true && sideB > sideC === true) && Math.pow(sideB, 2) === Math.pow(sideA, 2) + Math.pow(sideC, 2)  ||
-             (sideC > sideB === true && sideC > sideA === true) && Math.pow(sideC, 2) === Math.pow(sideB, 2) + Math.pow(sideA, 2)  ) {
-  typeOfTriangle = 'Right triangle';        
+             (a >  b === true && a > c === true) && Math.pow(a, 2) === Math.pow(b, 2) + Math.pow(c, 2) ||
+             (b > a === true && b > c === true) && Math.pow(b, 2) === Math.pow(a, 2) + Math.pow(c, 2)  ||
+             (c > b === true && c > a === true) && Math.pow(c, 2) === Math.pow(b, 2) + Math.pow(a, 2)  ) {
+  typeOfTriangle = 'Right triangle';         
 } else {
   typeOfTriangle = 'Scalene'; 
   }  
-if( ( ((typeof sideA) === 'number')  && ((typeof sideA) === 'number') && ((typeof sideA) === 'number')) &&
-   (sideA > 0 && sideB > 0 && sideC > 0) &&
-   (sideA + sideB > sideC && sideB + sideC > sideA & sideA + sideC > sideB) ) {
-   perymetrOfTriangle = sideA + sideB + sideC;
-   squareOfTriangle = Math.sqrt(perymetrOfTriangle*(perymetrOfTriangle - sideA)*(perymetrOfTriangle - sideB)*(perymetrOfTriangle - sideC));
- } else { 
-    console.log('Incorrect data');
-}
-
-
-console.log("Type of triangle is " + typeOfTriangle + " triangle and square is " + squareOfTriangle.toFixed(2) );
+    
+if(squareOfTriangle){
+   console.log("Type of triangle is " + typeOfTriangle + " triangle and square is " + squareOfTriangle.toFixed(2) );
+   }
+   else {
+     console.log('Incorrect data');
+   }
